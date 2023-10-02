5c6e33bf06e34b8e63916aaab372eaae2e4e378a
kjaya163 <68645300+kjaya163@users.noreply.github.com>
2023-10-02 19:47:56 +0530

    Add files via upload

---
+++ src/main/java/com/thinkconstructive/restdemo/controller/CloudVendorController.java
@@ -0,0 +1,54 @@
+package com.thinkconstructive.restdemo.controller;
+
+
+import com.thinkconstructive.restdemo.model.CloudVendor;
+import com.thinkconstructive.restdemo.response.ResponseHandler;
+import com.thinkconstructive.restdemo.service.CloudVendorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorController {
+
+    CloudVendorService cloudVendorService;
+    public CloudVendorController(CloudVendorService cloudVendorService) {
+        this.cloudVendorService = cloudVendorService;
+    }
+
+    // Read Specific Vendor Details
+    @GetMapping("{vendorId}")
+    public ResponseEntity<Object> getCloudVendorDetails(@PathVariable("vendorId") String vendorId){
+        return ResponseHandler.responseBuilder(
+                "ReQuested Vendor Details are given here",
+                HttpStatus.OK,
+                cloudVendorService.getCloudVendor(vendorId));
+    }
+
+    // Read All Vendor details
+    @GetMapping()
+    public List<CloudVendor> getAllCloudVendorDetails(){
+        return cloudVendorService.getAllCloudVendors();
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        cloudVendorService.createCloudVendor(cloudVendor);
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        cloudVendorService.updateCloudVendor(cloudVendor);
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(@PathVariable("vendorId") String vendorId){
+        cloudVendorService.deleteCloudVendor(vendorId);
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
---
+++ src/main/java/com/thinkconstructive/restdemo/service/impl/CloudVendorServiceImpl.java
@@ -0,0 +1,49 @@
+package com.thinkconstructive.restdemo.service.impl;
+
+import com.thinkconstructive.restdemo.exception.CloudVendorNotFoundException;
+import com.thinkconstructive.restdemo.model.CloudVendor;
+import com.thinkconstructive.restdemo.repository.CloudVendorRepository;
+import com.thinkconstructive.restdemo.service.CloudVendorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CloudVendorServiceImpl implements CloudVendorService {
+
+
+    CloudVendorRepository cloudVendorRepository;
+    public CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Successfully Inserted";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        cloudVendorRepository.save(cloudVendor);
+        return "Successfully Updated";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        cloudVendorRepository.deleteById(cloudVendorId);
+        return "Deleted Successfully";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        if(cloudVendorRepository.findById(cloudVendorId).isEmpty())
+            throw new CloudVendorNotFoundException("Requested Cloud Vendor Does Not Exist");
+        return cloudVendorRepository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        return cloudVendorRepository.findAll();
+    }
+}
